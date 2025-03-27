a7c77ee432d8a2bae594fecb721d300e2ef7d1fb
Jaimin <jaimin.m@browserstack.com>
2025-03-27 15:43:06 +0530

    added new flaky test

---
+++ src/test/java/com/qdbrowserstack/MultipleTest.java
@@ -0,0 +1,52 @@
+package com.qdbrowserstack;
+
+import BaseDriver.DriverSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+public class MultipleTest extends DriverSetup {
+
+    @Test
+    public void addProductToCart() throws Exception {
+        driver.get("https://www.bstackdemo.com");
+
+        // Check the title
+        Assert.assertTrue(driver.getTitle().matches("StackDemo"));
+
+        // Save the text of the product for later verify
+        String productOnScreenText = driver.findElement(By.xpath("//*[@id=\"1\"]/p")).getText();
+        // Click on add to cart button
+        driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click();
+
+        // See if the cart is opened or not
+        Assert.assertTrue(driver.findElement(By.cssSelector(".float\\-cart__content")).isDisplayed());
+
+        // Check the product inside the cart is same as of the main page
+        String productOnCartText = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).getText();
+        Assert.assertEquals(productOnScreenText, productOnCartText);
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Product matched\"}}");
+    }
+
+    @Test(invocationCount = 2)
+    public void flakyTest(ITestContext testContext) throws Exception {
+        int currentCount = testContext.getAllTestMethods()[0].getCurrentInvocationCount();
+        System.out.println("Executing count: " + currentCount);
+        driver.get("https://www.bstackdemo.com");
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        // Check the title
+        if(currentCount == 0) {
+            System.out.println(currentCount);
+            Assert.assertTrue(driver.getTitle().matches("StackDemo"));
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Title matched!\"}}");
+
+        }else {
+            Assert.assertTrue(driver.getTitle().matches("Failure"));
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \"Title not matched\"}}");
+        }
+    }
+}
+
