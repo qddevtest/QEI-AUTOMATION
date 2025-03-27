f7b65df6dfea4984cb1061aeea976784dc9b4562
Jaimin <jaimin.m@browserstack.com>
2025-03-27 16:46:27 +0530

    added fix

--- src/test/java/com/qdbrowserstack/MultipleTest.java
+++ src/test/java/com/qdbrowserstack/MultipleTest.java
@@ -10,6 +10,27 @@ import org.testng.annotations.Test;
 public class MultipleTest extends DriverSetup {
 
     @Test
+    public void passTest() throws Exception {
+        driver.get("https://www.bstackdemo.com");
+
+        // Check the title
+        Assert.assertTrue(driver.getTitle().matches("StackDemo"));
+
+        // Save the text of the product for later verify
+        String productOnScreenText = driver.findElement(By.xpath("//*[@id=\"1\"]/p")).getText();
+        // Click on add to cart button
+        driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click();
+
+        // See if the cart is opened or not
+        Assert.assertTrue(driver.findElement(By.cssSelector(".float\\-cart__content")).isDisplayed());
+
+        // Check the product inside the cart is same as of the main page
+        String productOnCartText = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).getText();
+        Assert.assertEquals(productOnScreenText, productOnCartText);
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Product matched\"}}");
+    }
+    @Test
     public void flakyTest() throws Exception {
         driver.get("https://www.bstackdemo.com");
 
